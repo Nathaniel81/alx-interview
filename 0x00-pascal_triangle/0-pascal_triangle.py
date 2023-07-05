af719e9d2cea03cc5a4d17a8ac8174d3ec5dfb72
Nathaniel81 <104891061+Nathaniel81@users.noreply.github.com>
2023-07-05 11:18:01 +0300

    Create 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Doc
+"""
+
+
+def pascal_triangle(n):
+    """R
+    eturns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
+    pascal = []
+    if n <= 0:
+        return pascal
+
+    for i in range(n):
+        inner = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                inner.append(1)
+                continue
+            top_inner = pascal[i - 1]
+            value = top_inner[j - 1] + top_inner[j]
+            inner.append(value)
+
+        pascal.append(inner)
+
+    return pascal

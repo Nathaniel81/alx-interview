f2361e83d83e661e9a1915c660ff4a58ad8a5b40
Nathaniel81 <104891061+Nathaniel81@users.noreply.github.com>
2023-07-13 20:34:34 +0300

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+0-minoperations module
+"""
+import math
+
+
+def get_largest_factor(n):
+    """
+    returns the largest factor of a number excluding the number itself
+    """
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return n // i
+    return 1
+
+
+def minOperations(n):
+    """
+    returns the minium number of operations required to generate n consecutive
+    'H' characters using 'Copy All' and 'paste' operations. If n is impossible
+    to achieve returns 0
+    """
+    if n <= 1:
+        return 0
+    largest_factor = get_largest_factor(n)
+    if largest_factor == 1:
+        return n
+    return n // largest_factor + minOperations(largest_factor)

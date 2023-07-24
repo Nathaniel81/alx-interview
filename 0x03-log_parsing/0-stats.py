feb48f8f01d8c568cabf3048344b5d3017ea8ac4
Nathaniel81 <104891061+Nathaniel81@users.noreply.github.com>
2023-07-24 21:47:42 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,45 @@
 #!/usr/bin/python3
-"""0-stats module
 """
-import sys
+Module 0-status.py
+"""
+from sys import stdin
 
 
-stats = {
-    '200': 0, '301': 0, '400': 0, '401': 0,
-    '403': 0, '404': 0, '405': 0, '500': 0
-}
-total = 0
-count = 0
+def status_printer(total_size, status):
+    """A method to print the status with the format given"""
+    print('File size: {}'.format(total_size))
+    for code, count in sorted(status.items()):
+        if count > 0:
+            print('{}: {:d}'.format(code, count))
 
 
-def print_stats(stats, total):
-    """print_stats function
-    """
-    print("File size: {}".format(total))
-    for key, value in sorted(stats.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
-
-
-if __name__ == "__main__":
+def main():
+    """Main method"""
+    status_codes = {'200': 0, '301': 0, '400': 0,
+                    '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    total = 0
+    count = 0
     try:
-        for line in sys.stdin:
-            data = line.split()
-            if len(data) > 4:
-                status = data[-2]
-                if status in stats.keys():
-                    stats[status] += 1
-                total += int(data[-1])
-                count += 1
-            if count == 10:
-                count = 0
-                print_stats(stats, total)
-    except Exception:
-        pass
-    finally:
-        print_stats(stats, total)
+        for line in stdin:
+            splited = line.split()
+            if count % 10 == 0 and count != 0:
+                status_printer(total_size=total, status=status_codes)
+            try:
+                code = splited[-2]
+                if code in status_codes.keys():
+                    status_codes[code] = status_codes[code] + 1
+            except Exception:
+                pass
+            try:
+                total += int(splited[-1])
+            except Exception:
+                pass
+            count += 1
+        status_printer(total_size=total, status=status_codes)
+    except KeyboardInterrupt:
+        status_printer(total, status_codes)
+        raise
+
+
+if __name__ == '__main__':
+    main()
